a0b5fb6f425abca224d2367f0cd5baf5d5b89abe
Taeyoung Won <hexter1994@gmail.com>
2023-01-27 02:05:55 +0900

    Project Init

---
+++ MVVM-ChattingApp/MVVM-ChattingApp/App/MVVM_ChattingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_ChattingAppApp.swift
+//  MVVM-ChattingApp
+//
+//  Created by 원태영 on 2023/01/27.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_ChattingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
